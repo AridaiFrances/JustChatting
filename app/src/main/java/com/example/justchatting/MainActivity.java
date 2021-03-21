1f887d5200320550f0bc1fa8210e74d198b7b2f0
aridescos <aridaifrances@gmail.com>
2021-03-21 20:44:14 +0100

    Función registro implementada.

--- app/src/main/java/com/example/justchatting/MainActivity.java
+++ app/src/main/java/com/example/justchatting/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     EditText entradaEmail, entradaPass;
     String lEmail;
     String lPass;
+    TextView forgot_password;
     private Button btnLogIn, btn_registro;
     private FirebaseAuth fautenticacion;
 
@@ -34,32 +36,39 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
+        fautenticacion = FirebaseAuth.getInstance();
 
         entradaEmail = (EditText) findViewById(R.id.entrada_email);
         entradaPass = (EditText) findViewById(R.id.entrada_pass);
         btnLogIn = (Button) findViewById(R.id.btn_login);
         btn_registro = (Button) findViewById(R.id.btn_registro);
-        fautenticacion = FirebaseAuth.getInstance();
-
-        fautenticacion.signOut();
+        forgot_password = findViewById(R.id.forgot_password);
 
 
-        // fautenticacion.signOut();
+
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //  startActivity(new Intent(MainActivity.this, forgotActivity.class));
+                Toast.makeText(MainActivity.this, "Función no implementada todavía",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                lEmail = entradaEmail.getText().toString().trim();
 
+                lEmail = entradaEmail.getText().toString().trim();
                 lPass = entradaPass.getText().toString().trim();
-//Lanzo metodo con los datos introducidos por el usuario
                 if (!TextUtils.isEmpty(lEmail) && !TextUtils.isEmpty(lPass)) {
 
                     logIn(lEmail, lPass);
 
 
                 } else {
-                    Toast.makeText(MainActivity.this, "Falta algún campo por rellenar",
+                    Toast.makeText(MainActivity.this, "Falta algún campo por rellenar, no ha sido posible iniciar sesión",
                             Toast.LENGTH_SHORT).show();
                 }
             }
@@ -68,7 +77,10 @@ public class MainActivity extends AppCompatActivity {
         btn_registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Todavía no tengo acción :(", Toast.LENGTH_SHORT).show();
+
+                Intent regIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(regIntent);
+
             }
         });
 
@@ -80,17 +92,13 @@ public class MainActivity extends AppCompatActivity {
         fautenticacion.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-//Si los datos introducidos son correctos, concedo acceso al usuario
                 if (task.isSuccessful()) {
                     Toast.makeText(MainActivity.this, "Login efectuado con éxito!", Toast.LENGTH_SHORT).show();
-                }
-//Si los datos son erroneos, lanzo el error corespondiente con un Toast.
-                else {
+                } else {
                     Toast.makeText(MainActivity.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
 
 }
